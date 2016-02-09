fc72c5302511da10b2136f282155009c08680524
K Kollmann <git@k.kollmann.moe>
2016-02-09 01:41:48 +0100

    add calculator files

---
+++ calculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  demoapp
+//
+//  Copyright (c) 2015 K Kollmann.
+//  All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var DisplayLabel: UILabel!
+    
+    @IBOutlet weak var DigitButton: UIButton!
+    
+    @IBOutlet weak var CalcButton: UIButton!
+    
+    @IBOutlet weak var ClearButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
